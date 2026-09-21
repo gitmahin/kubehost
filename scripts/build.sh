4f726b0ff15f6bce428691dd9a5701e882f24811
gitmahin <nimahin25@gmail.com>
2026-09-21 18:05:11 +0600

    chore: remove scripts

--- scripts/build.sh
+++
@@ -1,6 +0,0 @@
-docker build -t dockermahin/kubehost-server:latest -f ./apps/server/Dockerfile .
-
-docker build \
-  --build-arg VITE_API_SERVER_URL=http://kubehost.duckdns.org:3000/api \
-  -t dockermahin/kubehost-client:latest -f ./apps/web/Dockerfile .
-
